99214b955dda5e5ed4237cda0175ff093b5df247
Jonathan Price <jon.mark.price@gmail.com>
2015-12-01 18:04:34 -0600

    added unary minus

--- apps/postfix/parser.js
+++ apps/postfix/parser.js
@@ -1,47 +1,23 @@
-var eval_button = document.getElementById('eval');
-eval_button.onclick = function() {
+function display_result() {
   var textfield = document.getElementById('textfield');
   var text = textfield.value;
   textfield.value = '';
 
-	var tokens = text.trim().split(' ');
+  var tokens = text.trim().split(' ');
   var result = evaluate(tokens);
-	var resultItem = document.createElement('li');
-	var input = document.createElement('span');
-	input.classList.add('user_input');
-	input.textContent = text;
-	resultItem.appendChild(input);
+  var resultItem = document.createElement('li');
 
-	var resultSpan = document.createElement('span');
-	resultSpan.textContent = result;
-	resultItem.appendChild(resultSpan);
+  var input = document.createElement('span');
+  input.classList.add('user_input');
+  input.textContent = text;
+  resultItem.appendChild(input);
 
-	var resultList = document.getElementById('result');
-	resultList.firstElementChild
-	resultList.insertBefore(resultItem, resultList.firstElementChild);
-}
+  var resultSpan = document.createElement('span');
+  resultSpan.textContent = result;
+  resultItem.appendChild(resultSpan);
 
-function get_type(token) {
-  if (is_term(token) && !is_op(token)) {
-    return token + '.type = terminal\n';
-  }
-  else if (!is_term(token) && is_op(token)) {
-    return token + '.type = operator\n';
-  }
-  else {
-    return token + ' type unknown\n'
-  }
-}
-
-function parse_type(text) {
-  var tokens = text.split(' ');
-  //var stack = [];
-
-  var types = '';
-  for (var i = 0; i < tokens.length; ++i) {
-    types += get_type(tokens[i]);
-  }
-  return types;
+  var resultList = document.getElementById('result');
+  resultList.insertBefore(resultItem, resultList.firstElementChild);
 }
 
 function is_term(token) {
@@ -52,7 +28,7 @@ function is_term(token) {
 }
 
 function is_op(token) {
-  var ops = ['+', '-', '*', '/', '%', '^'];
+  var ops = ['+', '-', '*', '/', '%', '^', '~'];
   if (ops.indexOf(token) != -1) {
     return true;
   }
@@ -62,73 +38,81 @@ function is_op(token) {
 // Evaluation:
 var ops = {
   '+': {
-		symbol: '+',
-		arity: 2,
-		perform: function (a) { return a[1] + a[0]; }
-	},
+    symbol: '+',
+    arity: 2,
+    perform: function (a) { return a[1] + a[0]; }
+  },
   '*': {
-		symbol: '*',
-		arity: 2,
-		perform: function (a) { return a[1] * a[0]; }
-	},
-	'-': {
-		symbol: '-',
-		arity: 2,
-		perform: function (a) { return a[1] - a[0]; }
-	},
-	'/': {
-		symbol: '/',
-		arity: 2,
-		perform: function (a) { return a[1] / a[0]; }
-	},
-	'%': {
-		symbol: '%',
-		arity: 2,
-		perform: function (a) { return a[1] % a[0]; }
-	},
-	'^': {
-		symbol: '^',
-		arity: 2,
-		perform: function (a) { return Math.pow(a[1],a[0]); }
-	}
+    symbol: '*',
+    arity: 2,
+    perform: function (a) { return a[1] * a[0]; }
+  },
+  '~': {
+    symbol: '~',
+    arity: 1,
+    perform: function (a) { return -a[0]; }
+  },
+  '-': {
+    symbol: '-',
+    arity: 2,
+    perform: function (a) { return a[1] - a[0]; }
+  },
+  '/': {
+    symbol: '/',
+    arity: 2,
+    perform: function (a) { return a[1] / a[0]; }
+  },
+  '%': {
+    symbol: '%',
+    arity: 2,
+    perform: function (a) { return a[1] % a[0]; }
+  },
+  '^': {
+    symbol: '^',
+    arity: 2,
+    perform: function (a) { return Math.pow(a[1], a[0]); }
+  }
 }
 
 function evaluate(tokens) {
-	var stack, op, arity, result, args;
-	stack = [];
-	for (var i = 0; i < tokens.length; i++) {
-		if (is_term(tokens[i])) {
-			stack.push(Number(tokens[i]));
-		}
-		else if (is_op(tokens[i])) {
-			op = ops[tokens[i]];
-			args = [];
-			for (var j = 0; j < op.arity; j++) {
-				var tmp = stack.pop();
-				console.log('argument ' + j + ' is '  + tmp);
-				if (!isNaN(Number(tmp))) {
-					args[j] = tmp;
-				}
-				else {
-					return 'Too few arguments for ' + op.symbol +  ' operator.\n' +
-					'Expected ' + op.arity + ', got ' + j + '.';
-				}
-			}
-			console.log('args is:')
-			console.log(args);
-			result = op.perform(args);
-			stack.push(result);
-		}
-		else {
-			return '"' + tokens[i] + '" is not a valid token.';
-		}
-		console.log(stack);
-	}
-	if (stack.length == 1) {
-		return stack.pop();
-	}
-	else {
-		return 'Invalid expression, evaluation ended with ' + stack.length +
-		' items on stack.';
-	}
+  var stack, op, arity, result, args;
+  stack = [];
+  for (var i = 0; i < tokens.length; i++) {
+    if (is_term(tokens[i])) {
+      stack.push(Number(tokens[i]));
+    }
+    else if (is_op(tokens[i])) {
+      op = ops[tokens[i]];
+      args = [];
+      for (var j = 0; j < op.arity; j++) {
+        var tmp = stack.pop();
+        console.log('argument ' + j + ' is '  + tmp);
+        if (!isNaN(Number(tmp))) {
+          args[j] = tmp;
+        }
+        else {
+          return 'Too few arguments for ' + op.symbol +  ' operator.\n' +
+          'Expected ' + op.arity + ', got ' + j + '.';
+        }
+      }
+      console.log('args is:')
+      console.log(args);
+      result = op.perform(args);
+      stack.push(result);
+    }
+    else {
+      return '"' + tokens[i] + '" is not a valid token.';
+    }
+    console.log(stack);
+  }
+  if (stack.length == 1) {
+    return stack.pop();
+  }
+  else {
+    return 'Invalid expression, evaluation ended with ' + stack.length +
+    ' items on stack.';
+  }
 }
+
+var eval_button = document.getElementById('eval');
+eval_button.onclick = display_result;
